90e2f5a7cbfb88a5ab83fec0e59c781958aac2e6
Xinhui Li <sucyli1017@gmail.com>
2022-08-11 10:45:05 -0400

    Add torch simulation data

---
+++ scripts/MISA-PyTorch/generate_data.m
@@ -0,0 +1,69 @@
+function [sim_siva,S,M,A,Y,X,wpca,w0,w1] = generate_data(seed,n_sample,n_dataset,n_source)
+
+N=n_sample; % number of samples
+M_Tot=n_dataset; % number of datasets
+K=ones(1,n_source); % number of sources
+V=sum(K);
+Acond=3; % 1 means orthogonal matrix
+SNR=(1+999)/1;
+
+sim_siva = sim_basic_SIVA(seed,K,V,M_Tot,N,Acond,SNR);
+
+S = sim_siva.S; % save
+M = sim_siva.M; % save
+A = sim_siva.A; % save
+Y = sim_siva.Y; % save
+X = sim_siva.genX(); % save
+
+ut = utils;
+n_pc = sim_siva.C(1); % same as number of sources
+[whtM, H] = ut.doMMGPCA(X, n_pc, 'WT');
+
+% Use relative gradient
+gradtype = 'relative';
+
+% Enable scale control
+sc = 1;
+
+% Turn off preprocessing (still removes the mean of the data)
+preX = false;
+
+w0 = ut.stackW({diag(pi/sqrt(3)./std(H,[],2))*eye(size(H,1))});
+
+gica1 = MISAK(w0, 1, {eye(size(H,1))}, {H}, ...
+                0.5*ones(n_pc,1), ones(n_pc,1), ones(n_pc,1), ...
+                gradtype, sc, preX);
+
+% the whitening matrix is an identity matrix, different from the whitening matrix from PCA
+% sphering turns off PCA
+[W1,wht] = icatb_runica(H,'weights',gica1.W{1},'ncomps',size(H,1),'sphering', 'off', 'verbose', 'off', 'posact', 'off', 'bias', 'on');
+std_W1 = std(W1*H,[],2); % Ignoring wht because Infomax run with 'sphering' 'off' --> wht = eye(comps)
+W1 = diag(pi/sqrt(3) ./ std_W1) * W1; 
+
+% RUN GICA using MISA: continuing from Infomax above...
+% Could use stochastic optimization, but not doing so because MISA does not implement bias weights (yet)...
+% gica1.stochastic_opt('verbose', 'off', 'weights', gica1.W{1}, 'bias', 'off');%, 'block', 1100);
+[wout,fval,exitflag,output] = ut.run_MISA(gica1,{W1});
+std_gica1_W1 = std(gica1.Y{1},[],2);
+gica1.objective(ut.stackW({diag(pi/sqrt(3) ./ std_gica1_W1)*gica1.W{1}})); % update gica1.W{1}
+
+% Combine MISA GICA with whitening matrices to initialize multimodal model
+% W = cellfun(@(w) w,whtM,'Un',0);
+W = cellfun(@(w) gica1.W{1}*w,whtM,'Un',0);
+W = cellfun(@(w,x) diag(pi/sqrt(3) ./ std(w*x,[],2))*w,W,X,'Un',0); % save
+wpca = ut.stackW(W(M));
+
+W0 = cell(size(A));
+W1 = cell(size(A));
+for mm = M
+    n_component = size(S{mm},2);
+    [u, s, v] = svd(randn(size(A{mm}(:,1:n_component)')),'econ');
+    [u1, s1, v1] = svd(randn(size(A{mm}(:,1:n_component)')),'econ');
+    W0{mm} = u*v';
+    W1{mm} = u1*v1';
+end
+
+w0 = ut.stackW(W0(M));
+w1 = ut.stackW(W1(M));
+
+end
---
+++ scripts/MISA-PyTorch/simulation.m
@@ -0,0 +1,38 @@
+close all; clear; clc;
+% @gsd: generate simulated data
+% @gsm: generate simulated mixing matrix
+
+addpath("/Users/xli77/Documents/MISA/scripts");
+addpath("/Users/xli77/Documents/MISA/scripts/toy_example/");
+addpath(genpath('/Users/xli77/Documents/gift/GroupICATv4.0c'));
+
+outpath = '/Users/xli77/Documents/MISA/MISA-data/torch';
+seed_list = [7, 14, 21];
+sample_list = [64, 256, 1024, 4096, 16384, 32768];
+dataset_list = [2, 12, 32, 100];
+source_list = [12, 32, 100];
+count=1;
+
+for seed = seed_list
+    for n_dataset = dataset_list
+        for n_source = source_list
+            if (n_dataset==32&&n_source==100) || (n_dataset==100 && n_source==32) || (n_dataset==100 && n_source==100)
+                continue
+            end
+            for n_sample = sample_list
+                if n_source > n_sample
+                    continue
+                end
+                disp([count, seed, n_sample, n_dataset, n_source]);
+                
+                [sim_siva,S,M,A,Y,X,wpca,w0,w1] = generate_data(seed,n_sample,n_dataset,n_source);
+                
+                sim_siva = struct(sim_siva);
+                fn = sprintf('sim-siva_dataset%d_source%d_sample%d_seed%d.mat',n_dataset,n_source,n_sample,seed);
+                save(fullfile(outpath,fn),'sim_siva','S','M','A','Y','X','wpca','w0','w1');
+                
+                count=count+1;
+            end
+        end
+    end
+end
